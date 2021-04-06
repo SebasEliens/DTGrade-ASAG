d49f52c0cb7a59d292b7c3702ce0cd0f640caa23
Sebas <sebas.eliens@gmail.com>
2021-04-06 10:52:41 -0300

    add data, configs and defaults

---
+++ utils/configuration.py
@@ -0,0 +1,17 @@
+from types import SimpleNamespace
+import os
+import yaml
+
+__default_config_file__ = os.path.join(os.path.dirname(__file__), '..', 'configs', 'defaults.yml')
+
+def load_configs_from_file(file_path):
+    configs = dict()
+    with open(file_path, 'r') as f:
+        y = yaml.load_all(f, Loader = yaml.FullLoader)
+        for d in y:
+            configs.update(d)
+    return configs
+
+__default__ = load_configs_from_file(__default_config_file__)
+__datafile__ =  os.path.join(os.path.dirname(__file__),'..',  __default__['data']['directory'],  __default__['data']['filename'])
+__default_model_path__ = __default__['training']['model_path']

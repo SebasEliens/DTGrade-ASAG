84476c1ba5dd2e9f763afcfb54bb04f8e9070464
Sebas <sebas.eliens@gmail.com>
2021-04-06 23:01:25 -0300

    use eval to compute and save results

--- eval.py
+++ eval.py
@@ -1,13 +1,12 @@
 from utils.configuration import __datafile__, default_eval_model_and_config
 from utils.data import DTGradeDataset
 from utils.evaluation import Predictor
+import os
 
 def eval():
     model, config = default_eval_model_and_config()
     dataset =  DTGradeDataset.from_xml(__datafile__, model_path=config['model_path'])
     dataset.test()
     predictor = Predictor(model, dataset)
-    df = predictor.predict_data()
-    j =  predictor.compute_metrics()
-    print(df)
-    print(j)
+    predictor.predictions_to_csv(os.path.join('results/test_predictions.csv'))
+    predictor.metrics_to_json(os.path.join('results/test_metrics.json'))
